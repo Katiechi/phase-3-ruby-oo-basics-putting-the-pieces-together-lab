846795080516f33f71561b4a97bf82534f23d29d
brian ambundo <brian.katiechi9@gmail.com>
2022-08-22 05:11:14 +0300

    commit tree 98f508fbf48a91f619c738e0a6c0f8992cee9753

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,43 @@
 # Make your shoe class here!
+class Shoe 
+
+    attr_writer :color
+    attr_writer :size
+    attr_writer :material
+    attr_accessor :condition
+   def initialize(brand)
+        @brand = brand
+   end 
+
+   def brand 
+    @brand
+   end
+
+   def color  
+    @color
+   end
+
+   def size 
+    @size
+   end
+
+   def material 
+    @material 
+   end
+
+   def condition 
+    @condition
+   end
+
+   def cobble
+    puts "Your shoe is as good as new!"
+    @condition= "new"
+   end
+end
+
+
+shoe = Shoe.new("Adidas")
+shoe.color = "red"
+shoe.size = 9.5
+shoe.material = "suede"
+shoe.condition = "tattered"
